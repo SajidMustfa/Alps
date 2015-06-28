fc00159e0f861cd24f27f7dd2cffeabb958b126f
Sajid <sajid1987@users.noreply.github.com>
2015-06-28 14:06:20 +0530

    Update Launch.java

--- Europa/src/main/java/org/lib/Launch.java
+++ Europa/src/main/java/org/lib/Launch.java
@@ -4,7 +4,7 @@ public class Launch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("hi all chikooossssvvvvv");
+			System.out.println("hi all chikooossssvvvvvccc");
 	}
 	
 	public String Ukey(){
